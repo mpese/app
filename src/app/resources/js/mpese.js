26cfce6a20f4318916b3be0c66f4cd2ef26158b0
Mike Jones <mike.a.jones@bristol.ac.uk>
2017-10-24 15:33:09 +0100

    Work on showing texts.

--- src/app/resources/js/mpese.js
+++ src/app/resources/js/mpese.js
@@ -11,3 +11,39 @@ var dashboard = {
 };
 
 dashboard.init();
+
+
+var text = {
+    init: function () {
+        $('ul#mpese-text-nav > li > a').off('click')
+        $('ul#mpese-text-nav > li > a').on('click', function (e) {
+            e.preventDefault();
+
+            var $active_tab = $(this).parent().attr('id');
+            var $tmp = $active_tab.split("-");
+            var $active_panel = $tmp[0].concat("-", $tmp[1], "-panel");
+
+            // loop through tabs
+            $tabs = $("#mpese-text-nav > li");
+            for (var i = 0; i < $tabs.length; i++) {
+                if ($($tabs[i]).attr('id') === $active_tab) {
+                    $($tabs[i]).addClass('active');
+                } else {
+                    $($tabs[i]).removeClass('active');
+                }
+            }
+
+            // loop through panels
+            $panels = $("div[data-panel]");
+            for (var i = 0; i < $panels.length; i++) {
+                if ($($panels[i]).attr('id') === $active_panel) {
+                    $($panels[i]).show();
+                } else {
+                    $($panels[i]).hide();
+                }
+            }
+        });
+    }
+}
+
+text.init()
