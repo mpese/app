fb71d4516cc5261f2d8dd98cf1883dcc86434a1a
Mike Jones <mike.a.jones@bristol.ac.uk>
2018-09-04 10:13:03 +0100

    Removed the JIRA feedback utility.

--- src/app/resources/js/mpese-feedback.js
+++
@@ -1,7 +0,0 @@
-// Requires jQuery!
-$.ajax({
-    url: "https://bristoluni.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/-cnvp79/b/12/e73395c53c3b10fde2303f4bf74ffbf6/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector-embededjs.js?locale=en-US&collectorId=9fb78f04",
-    type: "get",
-    cache: true,
-    dataType: "script"
-});
